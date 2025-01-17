3ef0870a7541d87510aa1814bdf29f35638b8c00
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-01-18 01:00:58 +0200

    [FEAT] Implement endpoint to retrieve all expenses for a user

--- src/app/api/api_v1/routes/expense_route.py
+++ src/app/api/api_v1/routes/expense_route.py
@@ -4,9 +4,30 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.sql_app.database import get_db
-from app.schemas.user import UserRegistration
-from app.services import user_service
+from app.services import expense_service
 from app.services import auth_service
 from app.services.utils.processors import process_request
 
 router = APIRouter()
+
+
+@router.get(
+    "/users/{user_id}",
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_expenses(
+    user_id: UUID,
+    db: AsyncSession = Depends(get_db),
+) -> JSONResponse:
+    """
+    Get all expenses for a user.
+    """
+
+    async def _get_user_expenses():
+        return await expense_service.get_user_expenses(user_id=user_id, db=db)
+
+    return await process_request(
+        get_entities_fn=_get_user_expenses,
+        status_code=status.HTTP_200_OK,
+        not_found_err_msg="User not found.",
+    )
--- src/app/services/expense_service.py
+++ src/app/services/expense_service.py
@@ -1,15 +1,46 @@
 import logging
-from typing import Union
 from uuid import UUID
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import status
 
-from app.schemas.user import UserRegistration, UserResponse
-from app.schemas.common.messages import ResponseMessage
-from app.services.utils import processors as p, validators as v, utils as u
+from app.schemas.expense import ExpenseResponse
+from app.services.utils import validators as v, utils as u
 from app.schemas.common.application_error import ApplicationError
-from app.sql_app.user.user import User
+from app.sql_app.expense.expense import Expense
+from app.sql_app.expense_name.expense_name import ExpenseName
 
 logger = logging.getLogger(__name__)
+
+
+async def get_user_expenses(user_id: UUID, db: AsyncSession) -> list[ExpenseResponse]:
+    """
+    Get all expenses for a user.
+
+    Args:
+        user_id (UUID): The user's unique identifier.
+        db (AsyncSession): The database session.
+
+    Returns:
+        List[ExpenseResponse]: A list of expenses for the user.
+    """
+    if not await v.user_exists(user_id=user_id, db=db):
+        raise ApplicationError(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found.",
+        )
+
+    user_expenses = await db.execute(select(Expense).filter(Expense.user_id == user_id))
+    expenses = user_expenses.scalars().all()
+
+    return [
+        ExpenseName(
+            id=expense.id,
+            name=expense.name,
+            amount=expense.amount,
+            date=expense.date,
+            created_at=expense.created_at,
+        )
+        for expense in expenses
+    ]
