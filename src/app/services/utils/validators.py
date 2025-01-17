066413bf9920b01cc4a70098a195999e562b9daa
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-01-18 01:01:35 +0200

    [FEAT] Add user_exists validator

--- src/app/services/utils/validators.py
+++ src/app/services/utils/validators.py
@@ -1,3 +1,4 @@
+from typing import Union
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -34,3 +35,19 @@ async def unique_email(email: str, db: AsyncSession) -> bool:
     result = await db.execute(select(User).filter(User.email == email))
     user = result.scalars().first()
     return user is None
+
+
+async def user_exists(user_id: str, db: AsyncSession) -> bool:
+    """
+    Checks if a user exists in the database.
+
+    Args:
+        user_id (str): The user_id to check.
+        db (AsyncSession): The database session.
+
+    Returns:
+        bool: True if the user is unique, False otherwise.
+    """
+    result = await db.execute(select(User).filter(User.id == user_id))
+    user: Union[User, None] = result.scalars().first()
+    return user is not None
