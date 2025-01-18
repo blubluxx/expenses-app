014d8a5b39b106c62d54b47ba3d4c7588ec602dd
Antoanet Simeonova <antoanet.simeonova@gmail.com>
2025-01-18 21:18:51 +0200

    [FEAT] Create ExpenseCreate model

--- src/app/schemas/expense.py
+++ src/app/schemas/expense.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from uuid import UUID
-from pydantic import BaseModel
+from dateutil import parser
+
+from pydantic import BaseModel, Field, field_validator
+
+from app.sql_app.expense_name.expense_name import ExpenseName
 
 
 class ExpenseResponse(BaseModel):
@@ -23,12 +27,65 @@ class ExpenseResponse(BaseModel):
 
     class Config:
         from_attributes = True
-        orm_mode = True
 
-    @property
-    def date(self) -> datetime:
-        return self.date.strftime("%d-%m-%Y %H:%M")
+    @field_validator("date")
+    def date(cls, value) -> str:
+        return value.strftime("%d-%m-%Y %H:%M")
 
-    @property
-    def created_at(self) -> datetime:
-        return self.date.strftime("%d-%m-%Y %H:%M")
+    @field_validator("created_at")
+    def created_at(cls, value) -> str:
+        return value.strftime("%d-%m-%Y %H:%M")
+
+    @field_validator("name")
+    def ensure_name_is_string(cls, value) -> str:
+        return value.name if isinstance(value, ExpenseName) else value
+
+
+class ExpenseCreate(BaseModel):
+    """
+    A Pydantic model for creating an expense.
+
+    Attributes:
+        name (str): The name of the expense.
+        amount (float): The amount of the expense.
+        date (datetime): The date the expense was incurred.
+        category (str): The category of the expense.
+    """
+
+    name: str = Field(examples=["Expense name"])
+    amount: float = Field(gt=0, examples=[100.0])
+    date: str = Field(
+        examples=["01/01/2022 12:00"], description="24h format is assumed"
+    )
+    category: str = Field(examples=["Category name"])
+
+    class Config:
+        from_attributes = True
+
+    @field_validator("date")
+    def date_from_string(cls, date_str: str) -> datetime:
+        """
+        Converts a date <str> in the format d/m/y (passed as decimals) to a <datetime> object
+        """
+
+        return parser.parse(date_str)
+
+
+class ExpenseNameDTO(BaseModel):
+    """
+    A Pydantic model for an expense name.
+
+    Attributes:
+        id (UUID): The expense name's unique identifier.
+        category_id (UUID): The unique identifier of the associated category.
+        user_id (UUID): The unique identifier of the user who created the expense name.
+        name (str): The name of the expense.
+    """
+
+    id: UUID
+    category_id: UUID
+    user_id: UUID
+    name: str
+
+    class Config:
+        from_attributes = True
